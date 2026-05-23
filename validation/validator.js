25b5b62d50305dfdbd562c3d1dc9d67e406fb448
woodrow-snow <woodrow2021@outlook.com>
2026-05-23 16:42:40 -0600

    made quick change to put validator

--- validation/validator.js
+++ validation/validator.js
@@ -54,27 +54,27 @@ const putModValidationRules = () => {
           body('name').trim().toLowerCase().isString().withMessage('Name must be a string'),
           body('copies')
                .trim()
-               .isNumeric()
-               .withMessage('Copies must only be numbers')
+            //    .isNumeric()
+            //    .withMessage('Copies must only be numbers')
                .isLength({ max: 2 })
                .withMessage('Copies can only be 2 digits big'),
           body('c_rank')
-               .trim()
-               .isNumeric()
-               .withMessage('c_rank must be number only')
-               .isLength({ max: 2 })
-               .withMessage('c_rank can only be 2 digits big'),
+               .trim(),
+            //    .isNumeric()
+            //    .withMessage('c_rank must be number only')
+            //    .isLength({ max: 2 })
+            //    .withMessage('c_rank can only be 2 digits big'),
           body('max_rank')
                .trim()
                .isLength({ max: 2 })
-               .withMessage('c_rank can only be 2 digits big')
-               .isNumeric()
-               .withMessage('c_rank must be number only'),
+               .withMessage('max_rank can only be 2 digits big'),
+            //    .isNumeric()
+            //    .withMessage('max_rank must be number only'),
           body('rarity').trim().isAlpha().withMessage('rarity must be letters only'),
           body('in_set')
                .trim()
-               .isIn(['yes', 'no'])
-               .withMessage("in_set must be either 'yes' or 'no'")
+            //    .isIn(['yes', 'no'])
+            //    .withMessage("in_set must be either 'yes' or 'no'")
      ];
 };
 
